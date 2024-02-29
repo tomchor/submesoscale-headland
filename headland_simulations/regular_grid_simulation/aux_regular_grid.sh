23a0b4912e9282c803ca25b63e1a161e8f4469de
tomchor <tomaschor@gmail.com>
2024-02-28 20:49:51 -0700

    add logs to regular_grid_simulations

--- headland_simulations/regular_grid_simulation/aux_regular_grid.sh
+++ headland_simulations/regular_grid_simulation/aux_regular_grid.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 #PBS -A UMCP0028
-#PBS -N R05F008-f2
-#PBS -o logs/NPN-R05F008-f2.log
-#PBS -e logs/NPN-R05F008-f2.log
+#PBS -N R05F02-f2
+#PBS -o logs/NPN-R05F02-f2.log
+#PBS -e logs/NPN-R05F02-f2.log
 #PBS -l walltime=24:00:00
 #PBS -q casper
 #PBS -l select=1:ncpus=1:ngpus=1
@@ -21,9 +21,9 @@ module li
 export JULIA_DEPOT_PATH="/glade/work/tomasc/.julia"
 echo $CUDA_VISIBLE_DEVICES
 
-time julia --project --pkgimages=no regular_headland.jl --simname=NPN-R05F008-f2 2>&1 | tee logs/NPN-R05F008-f2.out
-#time julia --check-bounds=no --pkgimages=no --project headland.jl --simname=NPN-R05F008-f2 2>&1 | tee logs/NPN-R05F008-f2.out
+time julia --project --pkgimages=no regular_headland.jl --simname=NPN-R05F02-f2 2>&1 | tee logs/NPN-R05F02-f2.out
+#time julia --check-bounds=no --pkgimages=no --project headland.jl --simname=NPN-R05F02-f2 2>&1 | tee logs/NPN-R05F02-f2.out
 
-qstat -f $PBS_JOBID >> logs/NPN-R05F008-f2.log
-qstat -f $PBS_JOBID >> logs/NPN-R05F008-f2.out
+qstat -f $PBS_JOBID >> logs/NPN-R05F02-f2.log
+qstat -f $PBS_JOBID >> logs/NPN-R05F02-f2.out
 
