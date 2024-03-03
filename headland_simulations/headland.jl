267a8821779a4b3da4222ed8e243678fcaae6c93
tomchor <tomaschor@gmail.com>
2024-03-03 11:31:37 -0700

    slightly decrease number of points since longest simulation are running in 4+ days now

--- headland_simulations/headland.jl
+++ headland_simulations/headland.jl
@@ -284,9 +284,9 @@ set!(model, b=(x, y, z) -> b∞(x, y, z, 0, f_params), v=params.V_inf)
 
 #+++ Create simulation
 params = (; params..., T_advective_max = params.T_advective_spinup + params.T_advective_statistics)
-simulation = Simulation(model, Δt=0.1*minimum_zspacing(grid.underlying_grid)/params.V_inf,
+simulation = Simulation(model, Δt=0.2*minimum_zspacing(grid.underlying_grid)/params.V_inf,
                         stop_time=params.T_advective_max * params.T_advective,
-                        wall_time_limit=23.2hours,
+                        wall_time_limit=23.3hours,
                         )
 
 using Oceanostics.ProgressMessengers
