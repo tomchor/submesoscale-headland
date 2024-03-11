ad419067959b6777fd853f1d836bca70425b1903
tomchor <tomaschor@gmail.com>
2024-03-11 15:29:15 -0600

    plot buoyancy mixing rate along with KE dissipation rate in hplot03

--- hplot03_dissipation_curves.py
+++ hplot03_dissipation_curves.py
@@ -7,12 +7,15 @@ from matplotlib import pyplot as plt
 from matplotlib.colors import LogNorm
 from cmocean import cm
 from scipy.optimize import curve_fit
+from aux02_plotting import letterize
 
 modifier = ""
 bulk = xr.open_dataset(f"data_post/bulkstats_snaps{modifier}.nc", chunks={})
 bulk = bulk.reindex(Ro_h = list(reversed(bulk.Ro_h)))
 
+variables = ["⟨ε̄ₖ⟩ˣᶻ", "⟨ε̄ₚ⟩ˣᶻ"]
 bulk["⟨ε̄ₖ⟩ˣᶻ"].attrs = dict(units="m²/s³")
+bulk["⟨ε̄ₚ⟩ˣᶻ"].attrs = dict(units="m²/s³")
 bulk.Slope_Bu.attrs =  dict(long_name=r"$S_{Bu} = Bu_h^{1/2} = Ro_h / Fr_h$")
 bulk.yC.attrs =  dict(long_name=r"$y$", units="m")
 
@@ -25,16 +28,15 @@ for buffer in bulk.buffer.values:
     print(f"Plotting buffer = {buffer} m")
     bulkb = bulk.sel(buffer=buffer)
     #+++ Create figure
-    ncols = 1
+    ncols = len(variables)
     nrows = 1
-    size = 3.5
+    size = 2.3*ncols
     fig, axes = plt.subplots(ncols=ncols, nrows=nrows,
-                             figsize = (1.4*ncols*size, nrows*size),
-                             sharex=False, sharey=False,
+                             figsize = (1.2*ncols*size, nrows*size),
+                             sharex = True, sharey = True,
                              squeeze = False,
                              constrained_layout=True)
     axesf = axes.flatten()
-    ax = axesf[0]
     #---
 
     cmap = plt.cm.copper_r
@@ -43,7 +45,8 @@ for buffer in bulk.buffer.values:
         for Fr_h in bulkb.Fr_h:
             bulk0 = bulkb.sel(Ro_h=Ro_h, Fr_h=Fr_h)
             S_normalized = (np.log10(bulk0.Slope_Bu) - np.log10(bulkb.Slope_Bu).min()) / (2*np.log10(bulkb.Slope_Bu).max())
-            bulk0["⟨ε̄ₖ⟩ˣᶻ"].pnplot(ax=ax, x="y", color=cmap(S_normalized))
+            for ax, variable in zip(axesf, variables):
+                bulk0[variable].pnplot(ax=ax, x="y", color=cmap(S_normalized))
 
     norm = LogNorm(vmin=bulkb.Slope_Bu.min().values, vmax=bulkb.Slope_Bu.max().values)
     sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
@@ -53,17 +56,16 @@ for buffer in bulk.buffer.values:
     #+++ Prettify and save
     for ax in axesf:
         ax.axvline(x=0, color="lightgray", ls="--", lw=1, zorder=0)
-        ax.set_xlim(-250, 1250)
+        ax.set_xlim(-250, bulk.yC[-1])
         ax.set_ylim(0, None)
-        ax.set_title(f"Average of KE dissipation rate\nexcluding {bulkb.buffer.item()} m closest to the boundary")
+        ax.set_title(f"")
+        #ax.set_title(f"Average of KE dissipation rate\nexcluding {bulkb.buffer.item()} m closest to the boundary")
 
         ax2 = ax.twinx()
         ax2.fill_between(bulkb.yC, headland_x_of_yz(bulkb.yC), color="lightgray", alpha=.8)
         ax2.set_ylim(0, 2e3)
         ax2.tick_params(left=False, right=False, bottom=False, labelleft=False, labelright=False, labelbottom=False)
+    letterize(axesf, x=0.05, y=0.9, fontsize=14)
 
-    if modifier:
-        fig.savefig(f"figures/dissipation_curves_buffer={buffer}m{modifier}.pdf")
-    else:
-        fig.savefig(f"figures/dissipation_curves_buffer={buffer}m.pdf")
+    fig.savefig(f"figures/dissipation_curves_buffer={buffer}m{modifier}.pdf")
     #---
