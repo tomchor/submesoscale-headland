6204d315c0ae55dad17bf1ee578ca81db72e3ca6
tomchor <tomaschor@gmail.com>
2024-03-16 08:32:12 -0600

    increased Lx by a factor of 25%

--- headland_simulations/siminfo.jl
+++ headland_simulations/siminfo.jl
@@ -31,6 +31,7 @@ end
                 Ro_h = 0.1,
 
                 β = 1, # headland_intrusion_size / headland_width = "bumpiness" of feature (β=1 for an x-y symmetric seamount, e.g.)
+                Lx_ratio = 2.5, # Lx / headland_intrusion_size_max
                 Ly_ratio = 15, # Ly / headland_width
 
                 Rz = 2.5e-3,
@@ -186,10 +187,10 @@ function expand_headland_parameters(params)
 
     #+++ Geometry
     Lz = 1.05* 2 * params.H # Lz is 5% larger than 2H in order to avoid flow constriction
-    headland_intrusion_size_max = 2*params.H / tan(θ_rad)
-    L = headland_intrusion_size_max / 2
+    headland_intrusion_size_max = 2*params.H / α
+    L = headland_intrusion_size_max / 2 # Horizontal length scale of the headland
 
-    Lx = 2.5 * headland_intrusion_size_max
+    Lx = params.Lx_ratio * headland_intrusion_size_max
     Ly = params.Ly_ratio * L
 
     sponge_length_y = params.sponge_fraction_Ly * Ly
