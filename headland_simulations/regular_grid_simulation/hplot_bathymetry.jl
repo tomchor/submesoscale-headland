27bc9e86fba8394a6fd5c040ed4609bb0544c8cf
tomchor <tomaschor@gmail.com>
2024-03-17 14:10:03 -0600

    improve hplot_bathymetry

--- headland_simulations/regular_grid_simulation/hplot_bathymetry.jl
+++ headland_simulations/regular_grid_simulation/hplot_bathymetry.jl
@@ -2,7 +2,7 @@ using Rasters
 import NCDatasets
 
 if !(@isdefined simname) || (typeof(simname) !== String)
-    simname = "NPN-R05F02-f2"
+    simname = "NPN-R1F008-f4"
 end
 
 @show "Reading NetCDF"
@@ -20,7 +20,7 @@ params = (; (Symbol(k) => v for (k, v) in md)...)
 #---
 
 @show "Slicing xyz"
-xyz = xyz[yC=Between(-md["runway_length"], Inf), xC=Between(-390, Inf)]
+xyz = xyz[yC=Between(-md["runway_length"], Inf), xC=Between(dims(PV, :xC)[2], Inf)]
 xC = Array(dims(xyz, :xC))
 yC = Array(dims(xyz, :yC))
 zC = Array(dims(xyz, :zC))
@@ -44,7 +44,7 @@ colormap[128-middle_chunk:128+middle_chunk] .= RGBAf(0,0,0,0)
 settings_axis3 = (aspect = (md["Lx"], md["Ly"], 4*md["Lz"]), azimuth = -0.80π, elevation = 0.2π,
                   perspectiveness=0.8, viewmode=:fitzoom, xlabel="x [m]", ylabel="y [m]", zlabel="z [m]")
 
-ax = Axis3(fig[1, 1]; settings_axis3...)
+ax = Axis3(fig[2, 1]; settings_axis3...)
 volume!(ax, xC, yC, zC, H, algorithm = :absorption, absorption=50f0, colormap = [:papayawhip, RGBAf(0,0,0,0), :papayawhip], colorrange=(-1, 1)) # turn on anti-aliasing
 
 vol = volume!(ax, xC, yC, zC, PVₙ, algorithm = :absorption, absorption=20f0, colormap=colormap, colorrange=PV_lims)
@@ -54,7 +54,7 @@ Colorbar(fig, vol, bbox=ax.scene.px_area,
 
 
 #+++ Inset axis
-inset_ax = Axis3(fig[1, 1]; width=Relative(0.4), height=Relative(0.4), halign=1, valign=1.1, zticks=[0, 40, 80], settings_axis3...)
+inset_ax = Axis3(fig[2, 1]; width=Relative(0.4), height=Relative(0.4), halign=1, valign=1.1, zticks=[0, 40, 80], settings_axis3...)
 contour!(inset_ax, xC, yC, zC, bathymetry, levels = [0], specular = Vec3f(0), diffuse = Vec3f(1), colormap = [:gray50], fxaa = true,) # turn on anti-aliasing
 
 ps = [Point3f(0, -400, 40)]
@@ -68,12 +68,25 @@ text!(Point3f(-200, -100 + params.Ly/2, 40),
       fontsize = 200, markerspace = :data, color = :brown2)
 #---
 
+#+++ Save a snapshot as png
 n[] = length(dims(PV, :Ti))
 save(string(@__DIR__) * "/../../figures/bathymetry_3d_PV_$simname.png", fig, px_per_unit=2);
+#---
 
+#+++ Define title with time
+using Printf
+using Oceananigans: prettytime
+title = @lift "Frₕ = $(@sprintf "%.2g" params.Fr_h),    Roₕ = $(@sprintf "%.2g" params.Ro_h);    " *
+              "Time = $(@sprintf "%s" prettytime(dims(PV, :Ti)[$n]))"
+fig[1, 1] = Label(fig, title, fontsize=18, tellwidth=false, height=8)
+#---
+
+pause
+#+++ Record animation
 n[] = 1
 frames = 1:length(dims(PV, :Ti))
 GLMakie.record(fig, string(@__DIR__) * "/../../anims/bathymetry_3d_PV_$simname.mp4", frames, framerate=14) do frame
     @info "Plotting time step $frame"
     n[] = frame
 end
+#---
