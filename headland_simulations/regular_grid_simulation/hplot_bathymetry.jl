33c52eb8a4e5a7cccdfb00ed0292246c57f3f457
tomchor <tomaschor@gmail.com>
2024-03-17 19:52:19 -0600

    modify hplot to plot two slightly differently rotated images

--- headland_simulations/regular_grid_simulation/hplot_bathymetry.jl
+++ headland_simulations/regular_grid_simulation/hplot_bathymetry.jl
@@ -30,7 +30,7 @@ PV_lims = (-PV_lim, +PV_lim)
 H = [ bathymetry(x, y, z) < 5 ? 1 : 0 for x=xC, y=yC, z=zC ]
 
 using GLMakie
-fig = Figure(resolution = (1200, 900));
+fig = Figure(resolution = (1600, 700));
 n = Observable(1)
 
 @show "Slicing PV"
@@ -41,32 +41,21 @@ colormap = to_colormap(:balance)
 middle_chunk = ceil(Int, 1.5 * 128 / PV_lim) # Needs to be *at least* larger than 128 / PV_lim
 colormap[128-middle_chunk:128+middle_chunk] .= RGBAf(0,0,0,0)
 
-settings_axis3 = (aspect = (md["Lx"], md["Ly"], 4*md["Lz"]), azimuth = -0.80π, elevation = 0.2π,
+function plot_image!(fig, row, col; aspect = (md["Lx"], md["Ly"], 4*md["Lz"]), azimuth = -0.80π, elevation = 0.2π,
                   perspectiveness=0.8, viewmode=:fitzoom, xlabel="x [m]", ylabel="y [m]", zlabel="z [m]")
 
-ax = Axis3(fig[2, 1]; settings_axis3...)
-volume!(ax, xC, yC, zC, H, algorithm = :absorption, absorption=50f0, colormap = [:papayawhip, RGBAf(0,0,0,0), :papayawhip], colorrange=(-1, 1)) # turn on anti-aliasing
+    ax = Axis3(fig[row, col]; aspect, azimuth, elevation, perspectiveness, viewmode, xlabel, ylabel, zlabel)
+    volume!(ax, xC, yC, zC, H, algorithm = :absorption, absorption=50f0, colormap = [:papayawhip, RGBAf(0,0,0,0), :papayawhip], colorrange=(-1, 1)) # turn on anti-aliasing
 
-vol = volume!(ax, xC, yC, zC, PVₙ, algorithm = :absorption, absorption=20f0, colormap=colormap, colorrange=PV_lims)
-Colorbar(fig, vol, bbox=ax.scene.px_area,
-         label="PV / N²∞ f₀", height=25, width=Relative(0.35), vertical=false,
-         alignmode = Outside(10), halign = 0.15, valign = 0.02)
+    vol = volume!(ax, xC, yC, zC, PVₙ, algorithm = :absorption, absorption=20f0, colormap=colormap, colorrange=PV_lims)
+    Colorbar(fig, vol, bbox=ax.scene.px_area,
+             label="PV / N²∞ f₀", height=25, width=Relative(0.35), vertical=false,
+             alignmode = Outside(10), halign = 0.15, valign = 0.02)
+    return ax
+end
 
-
-#+++ Inset axis
-inset_ax = Axis3(fig[2, 1]; width=Relative(0.4), height=Relative(0.4), halign=1, valign=1.1, zticks=[0, 40, 80], settings_axis3...)
-contour!(inset_ax, xC, yC, zC, bathymetry, levels = [0], specular = Vec3f(0), diffuse = Vec3f(1), colormap = [:gray50], fxaa = true,) # turn on anti-aliasing
-
-ps = [Point3f(0, -400, 40)]
-ns = [Vec3f(0, params.Ly/2, 0)]
-arrows!(inset_ax, ps, ns,
-        fxaa = true, # turn on anti-aliasing
-        color = :brown2, linewidth = 12, arrowsize = Vec3f(100, 20, 100))
-
-text!(Point3f(-200, -100 + params.Ly/2, 40),
-      text = "V∞ = 1 cm/s", rotation = 1.5π, align = (:left, :baseline),
-      fontsize = 200, markerspace = :data, color = :brown2)
-#---
+axl = plot_image!(fig, 2, 1, azimuth=-0.80π)
+axr = plot_image!(fig, 2, 2, azimuth=-0.82π)
 
 #+++ Save a snapshot as png
 n[] = length(dims(PV, :Ti))
