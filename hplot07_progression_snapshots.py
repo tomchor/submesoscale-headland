b2ad294cb9232166fc6aae100066418505cd527f
tomchor <tomaschor@gmail.com>
2024-03-20 13:54:30 -0600

    improve hplot07 to show both dissipation rate and PV

--- hplot07_progression_snapshots.py
+++ hplot07_progression_snapshots.py
@@ -7,11 +7,8 @@ from cmocean import cm
 from aux00_utils import open_simulation
 from aux02_plotting import letterize
 
-plt.rcParams['figure.constrained_layout.use'] = True
-
-modifier = ""
-variable = "PV_norm"
-#variable = "εₖ"
+modifier = "-f2"
+variables = ["PV_norm", "εₖ"]
 Fr_h = 0.08
 Ro_h = 0.2
 
@@ -49,106 +46,73 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-if True:
-    xyz["land_mask"] = xyz["Δxᶜᶜᶜ"].where(xyz["Δxᶜᶜᶜ"] == 0)
-    xyz["PV_norm"] = xyz.PV / (xyz.N2_inf * xyz.f_0)
+xyz["land_mask"] = xyz["Δxᶜᶜᶜ"].where(xyz["Δxᶜᶜᶜ"] == 0)
+xyz["PV_norm"] = xyz.PV / (xyz.N2_inf * xyz.f_0)
 
-    xyz = xyz.sel(time=np.inf, method="nearest").sel(xC=slice(-50, None), yC=slice(-250, 750))
-    ds_xz = xyz.sel(yC=[0, 50, 100, 150, 200, 250,], method="nearest")
-    #ds_xz = xyz.sel(yC=[0, 100, 200,], method="nearest")
+xyz = xyz.sel(time=np.inf, method="nearest").sel(xC=slice(-50, None), yC=slice(-250, 750))
+#ds_xz = xyz.sel(yC=[0, 50, 100, 150, 200, 250,], method="nearest")
+ds_xz = xyz.sel(yC=[0, 100, 200,], method="nearest")
 
-    ds_xz.PV_norm.attrs = dict(long_name=r"Ertel PV / $N^2_\infty f_0$")
+ds_xz.PV_norm.attrs = dict(long_name=r"Ertel PV / $N^2_\infty f_0$")
+ds_xz["εₖ"].attrs = dict(long_name=r"$\varepsilon_k$ [m²/s³]")
+ds_xz.xC.attrs["long_name"] = "$x$"
+ds_xz.zC.attrs["long_name"] = "$z$"
 
-    fg = ds_xz[variable].pnplot(x="x", col="y", col_wrap=3,
-                                cbar_kwargs = dict(shrink=0.9, fraction=0.03, pad=0.1, aspect=30, location="bottom",),
-                                figsize=(15, 6), rasterized=True, **plot_kwargs_by_var[variable],
-                                )
-    fig = plt.gcf()
+#+++ Create figure
+ncols = len(ds_xz.yC)
+nrows = len(variables)
+size = 2.5
+fig, axes = plt.subplots(nrows=2, ncols=ncols, figsize=(1.5*size*ncols+4, size*nrows),
+                         sharex=True, sharey=True)
+#---
 
-    opts_land = dict(cmap="Set2_r", vmin=0, vmax=1, alpha=1.0, zorder=10)
-    for i, ax in enumerate(fg.axs.flat):
-        if ax.get_xlabel():
-            ax.set_xlabel(ax.get_xlabel() + " [m]")
-        if ax.get_ylabel():
-            ax.set_ylabel(ax.get_ylabel() + " [m]")
-        if ax.get_title():
-            ax.set_title(ax.get_title() + " m")
+#+++ Plot stuff
+pcms = []
+for i, yC in enumerate(ds_xz.yC.values):
+    for j, variable in enumerate(variables):
+        ax = axes[j, i]
+        pcm = ds_xz[variable].sel(yC=yC).pnplot(ax=ax, x="x", **plot_kwargs_by_var[variable], add_colorbar=False, rasterized=True)
+        pcms.append(pcm)
+#---
 
-        ax.contour(ds_xz.xC, ds_xz.zC, ds_xz.b.pnisel(y=i), levels=15, colors="black", linestyles="--", linewidths=0.3)
+#+++ Plot bathymetry and adjust panels
+opts_land = dict(cmap="Set2_r", vmin=0, vmax=1, alpha=1.0, zorder=10)
+for i, ax_col in enumerate(axes.T):
+    ax_col[0].set_title(f"{ds_xz.yC.values[i]:.2f} m")
+    ax_col[1].set_title(f"")
+
+    ax_col[0].set_xlabel("")
+    ax_col[0].contour(ds_xz.xC, ds_xz.zC, ds_xz.b.pnisel(y=i), levels=15, colors="black", linestyles="--", linewidths=0.3)
+    for ax in ax_col:
         ax.pcolormesh(ds_xz.xC, ds_xz.zC, ds_xz.land_mask.pnisel(y=i), rasterized=True, **opts_land)
+        if j>0:
+            ax.set_ylabel("")
+fig.tight_layout()
+#---
 
-    #+++ Include horizontal plot on the right:
-    if True:
-        fig.subplots_adjust(right=0.8, bottom=0.2)
-        ax = fig.add_axes((0.86, 0.1, 0.12, 0.85))
+#+++ Include horizontal plot on the right:
+if True:
+    fig.subplots_adjust(right=0.8,)
+    ax = fig.add_axes((0.86, 0.1, 0.12, 0.85))
 
-        ds_xy = xyz.sel(zC=40, method="nearest")
-        ds_xy[variable].pnplot(ax=ax, x="x", add_colorbar=False, rasterized=True, **plot_kwargs_by_var[variable])
+    variable = variables[0]
+    ds_xy = xyz.sel(zC=40, method="nearest")
+    ds_xy[variable].pnplot(ax=ax, x="x", add_colorbar=False, rasterized=True, **plot_kwargs_by_var[variable])
 
-        ax.set_title("z = 40 m")
-        ax.set_xlabel("x [m]")
-        ax.set_ylabel("y [m]")
+    ax.set_title(f"z = {ds_xy.zC.values:.2f} m")
+    ax.set_xlabel("x [m]")
+    ax.set_ylabel("y [m]")
 
-        ax.pcolormesh(ds_xy.xC, ds_xy.yC, ds_xy.land_mask, rasterized=True, **opts_land)
-        for y in ds_xz.yC.values:
-            ax.axhline(y=y, ls="--", lw=0.5, color="k")
-    #---
+    ax.pcolormesh(ds_xy.xC, ds_xy.yC, ds_xy.land_mask, rasterized=True, **opts_land)
+    for y in ds_xz.yC.values:
+        ax.axhline(y=y, ls="--", lw=0.5, color="k")
 
-    letterize(np.array([*fg.axs.flat, ax]), x=0.05, y=0.9)
+    for i in range(len(variables)):
+        cax = axes[i,0].inset_axes([150, 5, 200, 5], transform=axes[i,0].transData, zorder=100)
+        c_ax = fig.colorbar(pcms[i], cax=cax, orientation="horizontal", label=ds_xz[variables[i]].attrs["long_name"], location="top")
+#---
 
-else:
-
-    #+++ Plot two panel at the same y value
-    slice_name = "xiz"
-    snaps = xr.open_dataset(f"data_post/{slice_name}_snaps{modifier}.nc")
-    ds = snaps.isel(time=-1).pnsel(x=slice(-150, None), Fr_h=0.08, Ro_h=0.2)
-
-    ds.PV_norm.attrs = dict(long_name=r"Ertel PV / $N^2_\infty f_0$")
-
-
-    #+++
-    nrows=2
-    ncols=1
-    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, squeeze=False,
-                             sharex=True, sharey=True,
-                             constrained_layout = True)
-    axesf = axes.flatten()
-
-    ax=axesf[0]
-    ds.PV_norm.pnplot(ax=ax, x="x", vmin=-4, vmax=4, cmap=cm.balance,
-                        rasterized = True,
-                        cbar_kwargs = dict(label="PV/$N^2_\infty f_0$"))
-    ax.set_xlabel("")
-
-    ax=axesf[1]
-    ds["εₖ"].pnplot(ax=ax, x="x", norm=LogNorm(vmin=5e-11, vmax=2e-9, clip=True), cmap="inferno",
-                         rasterized=True,
-                         cbar_kwargs = dict(label="KE dissipation rate [m²/s³]"),
-                         )
-
-    if nrows > 2:
-        ax=axesf[2]
-        ds["Ri"].pnplot(ax=ax, x="x", vmin=-1, vmax=1, cmap="RdBu_r",
-                             rasterized=True,
-                             cbar_kwargs = dict(label="Richardson number"),
-                             )
-
-    for ax in axesf:
-        ds.b.pncontour(ax=ax, x="x", levels=20, colors="white", linestyles="--", linewidths=0.4)
-        #ax.grid(True, linestyle="--", linewidth=0.4)
-        ax.set_title("")
-    #---
-
-    #+++ Final touches
-    Bu_h = (ds.Ro_h / ds.Fr_h)**2
-    info = ", ".join((f"Roₕ = {ds.Ro_h.item():.3g}",
-                      f"Frₕ = {ds.Fr_h.item():.3g}",
-                      f"Buₕ = {Bu_h.item()}",
-                      "y = 400 m",
-                      ))
-    fig.suptitle(info)
-    #---
-    #---
+letterize(np.array([*axes.flatten(), ax]), x=0.05, y=0.9)
 
 #+++ Save
 fig.savefig(f"figures/{variable}_progression_{simname}.pdf", dpi=200)
