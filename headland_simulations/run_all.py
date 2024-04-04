ee958e15154f62d79d0dc7c69f50fa9322db6b3a
tomchor <tomaschor@gmail.com>
2024-04-03 18:39:37 -0600

    all simulations now only need two 24h cycles to run

--- headland_simulations/run_all.py
+++ headland_simulations/run_all.py
@@ -23,6 +23,7 @@ simnames = [#"NPN-TEST",
 from cycler import cycler
 names = cycler(name=simnames)
 resolutions = cycler(resolution = ["-f4", "-f2", ""])
+resolutions = cycler(resolution = ["",])
 rotations = cycler(rotation = ["", "-S",])
 rotations = cycler(rotation = ["",])
 simnames = [ nr["name"] + nr["rotation"] + nr["resolution"] for nr in rotations * resolutions * names ]
@@ -102,10 +103,7 @@ for simname in simnames:
     else:
         options1 = options1 | dict(cpu_type = "milan")
         options2 = options2 | dict(gpu_type = "a100")
-        if "F008" in simname:
-            cmd1 = f"JID1=`qsub {aux_filename}`; JID2=`qsub -W depend=afterok:$JID1 {aux_filename}`; JID3=`qsub -W depend=afterok:$JID2 {aux_filename}`; qrls $JID1"
-        else:
-            cmd1 = f"JID1=`qsub {aux_filename}`; JID2=`qsub -W depend=afterok:$JID1 {aux_filename}`; qrls $JID1"
+        cmd1 = f"JID1=`qsub {aux_filename}`; JID2=`qsub -W depend=afterok:$JID1 {aux_filename}`; qrls $JID1"
 
     options_string1 = ":".join([ f"{key}={val}" for key, val in options1.items() ])
     options_string2 = ":".join([ f"{key}={val}" for key, val in options2.items() ])
