ee79e95f876c4f5923de0aee1ba2423649b39472
tomchor <tomaschor@gmail.com>
2024-07-09 13:25:41 -0600

    normalize plots in hplot14 for esaier visualization

--- hplot14_ageo_vels.py
+++ hplot14_ageo_vels.py
@@ -29,14 +29,15 @@ except ValueError:
 cbar_kwargs = dict(location="right", shrink=0.5, fraction=0.012, pad=0.02, aspect=30)
 figsize = (8, 7)
 
-#plot_kwargs = dict(vmin=-0.005, vmax=0.005, cmap=plt.cm.RdBu_r, rasterized=True)
-plot_kwargs = dict(vmin=-0.02, vmax=0.02, cmap=BuRd, rasterized=True)
+plot_kwargs = dict(vmin=-4, vmax=4, cmap=BuRd, rasterized=True)
 #---
 
 #+++ Create ageostrophic variables and pick subset of simulations
-snaps["∂Uᵃ∂z"] = snaps["∂u∂z"] - snaps["∂Uᵍ∂z"]
+snaps["∂u∂z_norm"] = snaps["∂u∂z"] / snaps["∂u∂z"].std(("xC", "yC")) # Normalize
+snaps["∂Uᵍ∂z_norm"] = snaps["∂Uᵍ∂z"] / snaps["∂Uᵍ∂z"].std(("xC", "yC")) # Normalize
+snaps["∂Uᵃ∂z_norm"] = snaps["∂u∂z_norm"] - snaps["∂Uᵍ∂z_norm"]
 
-labels = ["$\partial u/\partial z$", f"$\partial U^a/\partial z$"]
+labels = ["$\partial u/\partial z$", f"$\partial u^a/\partial z$"]
 
 snaps = snaps.sel(Fr_h=Fr_h)
 snaps.xC.attrs = dict(long_name="$x$", units="m")
@@ -49,12 +50,12 @@ for j_Ro, Ro_h in enumerate(snaps.Ro_h.values):
     print(f"Plotting Roₕ = {Ro_h}")
 
     ax = axes[0, j_Ro]
-    im = snaps["∂u∂z"].sel(Ro_h=Ro_h).pnplot(ax=ax, x="x", add_colorbar=False, **plot_kwargs)
+    im = snaps["∂u∂z_norm"].sel(Ro_h=Ro_h).pnplot(ax=ax, x="x", add_colorbar=False, **plot_kwargs)
     ax.set_title(f"$Ro_h=$ {Ro_h}, $S_h=$ {Ro_h/Fr_h}")
     ax.set_xlabel("")
 
     ax = axes[1, j_Ro]
-    im = snaps["∂Uᵃ∂z"].sel(Ro_h=Ro_h).pnplot(ax=ax, x="x", add_colorbar=False, **plot_kwargs)
+    im = snaps["∂Uᵃ∂z_norm"].sel(Ro_h=Ro_h).pnplot(ax=ax, x="x", add_colorbar=False, **plot_kwargs)
     ax.set_title("")
 
     if j_Ro>0:
@@ -74,7 +75,7 @@ opts_land = dict(cmap="Set2", vmin=0, vmax=1, alpha=1.0, zorder=10,)
 for ax in axes.flatten():
     ax.pcolormesh(snaps.xC, snaps.yC, snaps.land_mask.where(snaps.land_mask==1), rasterized=True, **opts_land)
 
-fig.colorbar(im, ax=axes.ravel().tolist(), label="Vertical shear [1/s]", **cbar_kwargs)
+fig.colorbar(im, ax=axes.ravel().tolist(), label="Normalized vertical shear [-]", **cbar_kwargs)
 fig.suptitle("")
 fig.get_layout_engine().set(w_pad=0.02, h_pad=0, hspace=0, wspace=0)
 letterize(axes.flatten(), x=0.05, y=0.9)
