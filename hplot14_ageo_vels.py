57eb095cfbc571b6a6a424853d552665b9a1555e
tomchor <tomaschor@gmail.com>
2024-07-09 15:17:31 -0600

    switched to time-averaged outputs

--- hplot14_ageo_vels.py
+++ hplot14_ageo_vels.py
@@ -8,16 +8,16 @@ plt.rcParams["figure.constrained_layout.use"] = True
 plt.rcParams["font.size"] = 9
 
 modifier = ""
-slice_name = "xyi"
+slice_name = "tafields"
 Fr_h = 0.08
 
 #+++ Read and reindex dataset
-snaps = xr.open_dataset(f"data_post/{slice_name}_snaps{modifier}.nc").chunk(time="auto", Fr_h=1, Ro_h=1)
-#snaps = snaps.reindex(Ro_h = list(reversed(snaps.Ro_h)))
+snaps = xr.open_dataset(f"data_post/{slice_name}_snaps{modifier}.nc").chunk(Fr_h=1, Ro_h=1)
 snaps = snaps.sel(xC = slice(-snaps.headland_intrusion_size_max/3, np.inf),
                   yC = slice(-snaps.L, np.inf), Ro_h = slice(0.2, None))
 
-snaps = snaps.sel(time=30, method="nearest")
+if slice_name == "xyi":
+    snaps = snaps.sel(time=30, method="nearest")
 
 try:
     snaps = snaps.reset_coords(("zC", "zF"))
@@ -29,15 +29,20 @@ except ValueError:
 cbar_kwargs = dict(location="right", shrink=0.5, fraction=0.012, pad=0.02, aspect=30)
 figsize = (8, 7)
 
-plot_kwargs = dict(vmin=-4, vmax=4, cmap=BuRd, rasterized=True)
+plot_kwargs = dict(vmin=-3, vmax=+3, cmap=BuRd, rasterized=True)
 #---
 
 #+++ Create ageostrophic variables and pick subset of simulations
-snaps["∂u∂z_norm"] = snaps["∂u∂z"] / snaps["∂u∂z"].std(("xC", "yC")) # Normalize
-snaps["∂Uᵍ∂z_norm"] = snaps["∂Uᵍ∂z"] / snaps["∂Uᵍ∂z"].std(("xC", "yC")) # Normalize
+if slice_name == "xyi":
+    snaps["∂u∂z_norm"] = snaps["∂u∂z"] / snaps["∂u∂z"].std(("xC", "yC")) # Normalize
+    snaps["∂Uᵍ∂z_norm"] = snaps["∂Uᵍ∂z"] / snaps["∂Uᵍ∂z"].std(("xC", "yC")) # Normalize
+else:
+    snaps["∂u∂z_norm"] = snaps["∂ⱼūᵢ"].sel(i=1, j=3) / snaps["∂ⱼūᵢ"].sel(i=1, j=3).std(("xC", "yC")) # Normalize
+    snaps["∂Uᵍ∂z_norm"] = snaps["∂ⱼŪᵍᵢ"].sel(i=1, j=3) / snaps["∂ⱼŪᵍᵢ"].sel(i=1, j=3).std(("xC", "yC")) # Normalize
+
 snaps["∂Uᵃ∂z_norm"] = snaps["∂u∂z_norm"] - snaps["∂Uᵍ∂z_norm"]
 
-labels = ["$\partial u/\partial z$", f"$\partial u^a/\partial z$"]
+labels = ["$\partial \overline{u}/\partial z$", "$\partial \overline{u}^a/\partial z$"]
 
 snaps = snaps.sel(Fr_h=Fr_h)
 snaps.xC.attrs = dict(long_name="$x$", units="m")
