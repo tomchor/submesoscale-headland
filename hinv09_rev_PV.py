bce1ee122ace4304c67493afa82f01244f51c2a5
tomchor <tomaschor@gmail.com>
2024-11-25 05:37:39 -0700

    better plots to show overturning signal in PV

--- hinv09_rev_PV.py
+++ hinv09_rev_PV.py
@@ -7,16 +7,43 @@ from aux02_plotting import BuRd, letterize
 plt.rcParams["figure.constrained_layout.use"] = True
 plt.rcParams["font.size"] = 9
 
-xyi = xr.open_dataset("headland_simulations/data/xyi.NPN-R1F008.nc", decode_times=False).squeeze()
-xyi = xyi.assign_coords(time=xyi.time/xyi.T_advective)
+xyz = xr.open_dataset("headland_simulations/data/xyz.NPN-R1F008.nc", decode_times=False).squeeze()
+xyz = xyz.assign_coords(time=xyz.time/xyz.T_advective)
+xyz = xyz.sel(time=30)
 
-xy0 = xyi.sel(time=30)
-xy0["1 + Ro"] = 1 + xy0.Ro
+xyz["1 + Ro"] = 1 + xyz.Ro
+
+x0 = 200
+y0 = 700
+z0 = 40
+iyz = xyz.sel(xC=x0, method="nearest")
+xiz = xyz.sel(yC=y0, method="nearest")
+xyi = xyz.sel(zC=z0, method="nearest")
+
+fig, axes = plt.subplots(nrows=3, constrained_layout=True, sharey=True, figsize=(10, 10))
+yzsel = dict(yC=slice(600, 800), zC=slice(20, 60))
+iyz = iyz.sel(**yzsel)
+iyz.PV.pnplot(ax=axes[0], robust=True)
+iyz.u.pnplot(ax=axes[1], robust=True)
+iyz.Ri.pnplot(ax=axes[2], vmin=-2, vmax=2, cmap=BuRd)
+for ax in axes:
+    ax.axhline(y=z0, color="k", linestyle="--")
+    iyz.b.pncontour(ax=ax, y="z", levels=100, colors="green", linewidths=1,)
+
+fig, axes = plt.subplots(nrows=3, constrained_layout=True, sharey=True, figsize=(10, 10))
+xzsel = dict(x=slice(-50, 350), z=slice(20, 60))
+xiz.PV.pnsel(**xzsel).pnplot(ax=axes[0], robust=True)
+xiz.u.pnsel(**xzsel).pnplot(ax=axes[1], robust=True)
+xiz.Ri.pnsel(**xzsel).pnplot(ax=axes[2], vmin=-2, vmax=2, cmap=BuRd)
+for ax in axes:
+    ax.axhline(y=z0, color="k", linestyle="--")
+    xiz.b.pncontour(ax=ax, y="z", levels=30, colors="y", linewidths=1,)
 
 fig, axes = plt.subplots(ncols=3, constrained_layout=True, sharey=True, figsize=(10, 5))
-
-xsel = dict(x=slice(-50, 350), y=slice(500, 1200))
-xy0["1 + Ro"].pnsel(**xsel).pnplot(ax=axes[0], robust=True)
-xy0.u.pnsel(**xsel).pnplot(ax=axes[1], robust=True)
-xy0.Ri.pnsel(**xsel).pnplot(ax=axes[2], vmin=-2, vmax=2, cmap=BuRd)
+xysel = dict(x=slice(-50, 350), y=slice(500, 1200))
+xyi["1 + Ro"].pnsel(**xysel).pnplot(ax=axes[0], robust=True)
+xyi.u.pnsel(**xysel).pnplot(ax=axes[1], robust=True)
+xyi.Ri.pnsel(**xysel).pnplot(ax=axes[2], vmin=-2, vmax=2, cmap=BuRd)
+for ax in axes:
+    ax.axhline(y=y0, color="k", linestyle="--")
 
